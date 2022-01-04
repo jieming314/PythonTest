a083d3560932282abf367bf88471afde010f47ff
jieming314 <jimmy_zjmer@msn.com>
2022-01-04 14:56:59 +0800

    update multiple sanguo

--- multi/coroutine_lishiping.py
+++ multi/coroutine_lishiping.py
@@ -1,16 +1,17 @@
 '''
-使用进程池爬取梨视频科技页面的热门的3个视频
+使用异步io爬取梨视频科技页面的热门的3个视频
 '''
+
+import asyncio
+import aiohttp, aiofiles
 import os
-import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  #抑制InsecureRequestWarning 打印
 import time
-from multiprocessing import Pool
-from custom_requests import requests_get
 from lxml import etree
 import random
 import re
+from custom_requests import requests_get
 
 def retrieve_real_mp4_url(url):
     print(f'url is {url}')
@@ -34,45 +35,36 @@ def retrieve_real_mp4_url(url):
     response = requests_get(url=url_video,params=params,headers=headers)
     response.encoding='utf-8'
     fake_video_link = response.json()['videoInfo']['videos']['srcUrl']
-    # print(fake_video_link)
-
-    '''
-    从json中获取的到视频地址还需要经过处理才能得到真实的地址
-    https://video.pearvideo.com/mp4/adshort/20211228/1641015800122-15810627_adpkg-ad_hd.mp4     ----假地址
-    https://video.pearvideo.com/mp4/third/20211223/cont-1748493-10097838-230311-hd.mp4          ----真地址
-
-    https://video.pearvideo.com/mp4/third/20211223/1641012225100-10097838-230311-hd.mp4
-    https://video.pearvideo.com/mp4/adshort/20211228/cont-1706917-15810627_adpkg-ad_hd.mp4
-    '''
-
     p = re.compile(r'https://(.*)/(\d+?)-(.*)')
     s = r'https://\g<1>/cont-' + video_id + r'-\g<3>'   #这里使用了反向引用，每个分组对应\g<分组id>
     video_link = p.sub(s,fake_video_link)
 
-    # print(video_link)
-
     return video_link
 
-def download_video(video_tuple):
-
-    c_pid = os.getpid()
-
-    print(f'pid {c_pid} start download...')
+async def download_video(**video_dict):
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
     }
 
-    video_name = video_tuple[0]
-    url = video_tuple[1]
-    response = requests_get(url=url,headers=headers)
-    mp4_content = response.content
+    video_name = video_dict['name']
+    url = video_dict['url']
 
-    with open(video_name, 'wb') as fp:
-        fp.write(mp4_content)
+    print(f'video name is {video_name}')
+    print(f'video url is {url}')
 
-    print(f'pid {c_pid} download complete...')
+    async with aiohttp.ClientSession() as session:
+        if 'USERNAME' in os.environ and os.environ['USERNAME'] == 'jmzhang':
+            async with session.get(url,headers=headers) as response:
+                mp4_content = await response.read()
+        else:
+            async with session.get(url,headers=headers,proxy='http://10.158.100.9:8080') as response:
+                mp4_content = await response.read()
 
+    async with aiofiles.open(video_name,'wb') as fp:
+        await fp.write(mp4_content)
+    
+    print(f'{video_name} download complete...')
 
 if __name__=='__main__':
 
@@ -103,29 +95,26 @@ if __name__=='__main__':
         video_id = video_tag.xpath('.//a/@href')[0]
         video_link = 'https://www.pearvideo.com/' + video_id
         video_real_url = retrieve_real_mp4_url(video_link)
-        video_list.append((video_name,video_real_url))
+
+        video_dict = {
+            'name': video_name,
+            'url': video_real_url
+        }
+
+        video_list.append(video_dict)
 
     print(video_list)
 
-    '''
-    for循环串行
-    '''
-    for each in video_list:
-        download_video(each)
+    #异步协程方式下载mp4文件
+    loop = asyncio.get_event_loop()
+    tasks = []
+    for video_dict in video_list:
+        c = download_video(**video_dict)
+        task = loop.create_task(c)
+        tasks.append(task)
 
-    p = Pool()
-    '''
-    使用apply_async的进程池
-    '''
-    # for each in video_list:
-    #     p.apply_async(download_video,(each,))
-    # p.close()
-    # p.join()
-
-    '''
-    使用map的进程池
-    '''
-    # p.map(download_video, video_list)
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
 
     print('over!!!')
     
